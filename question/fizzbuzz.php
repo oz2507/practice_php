cdd557e26edae225db6ab5a7382906a943740657
oz2507 <oz.nytia07@gmai.com>
2018-09-08 17:40:00 +0900

    ファイル整理

--- question/fizzbuzz.php
+++ question/fizzbuzz.php
@@ -5,15 +5,3 @@
  * 3の倍数の時はFizzと出力、5の倍数の時はBuzzと出力してください。
  * 3と5の倍数の時はFizzBuzzと出力してください。
  */
-
-for ($i=1; $i < 101; $i++) { 
-	if ($i % 15 == 0) {
-		echo 'FizzBuzz' . PHP_EOL;
-	} elseif ($i % 3 == 0) {
-		echo "Fizz" . PHP_EOL;
-	} elseif ($i % 5 == 0) {
-		echo "Buzz" . PHP_EOL;
-	} else {
-		echo "$i" . PHP_EOL;
-	}
-}

17e11de514f08102ded16ba478f9183a2198b2a0
oz2507 <oz.nytia07@gmai.com>
2018-11-08 23:19:43 +0900

    int完了

---
+++ regular_expression/int.php
@@ -0,0 +1,68 @@
+<?php
+
+// 車のナンバーで真偽を判断
+$numbers = ['46-49', '49-49', '49-46', '46-46'];
+
+// 完全に一致するもののみ
+foreach ($numbers as $number) {
+		$result = preg_match("/46-49/", $number);
+		if ($result) {
+				echo "{$number}は条件に一致するナンバーです。<br>";
+		} else {
+				echo "{$number}は条件に一致しないナンバーです。<br>";
+		}
+}
+echo "<br>";
+
+// []を用いてを用いて範囲を指定
+foreach ($numbers as $number) {
+		$result = preg_match("/4[6-9]-49/", $number);
+		if ($result) {
+				echo "{$number}は条件に一致するナンバーです。<br>";
+		} else {
+				echo "{$number}は条件に一致しないナンバーです。<br>";
+		}
+}
+echo "<br>";
+
+// .はなんでもいい 
+foreach ($numbers as $number) {
+		$result = preg_match("/4.-49/", $number);
+		if ($result) {
+				echo "{$number}は条件に一致するナンバーです。<br>";
+		} else {
+				echo "{$number}は条件に一致しないナンバーです。<br>";
+		}
+}
+echo "<br>";
+
+
+// 電話番号の真偽を判断
+$phone_numbers = ['080-1234-5678', '090-1234-5678', '010-1234-5678', '09012345678'];
+
+foreach ($phone_numbers as $phone_number) {
+		$result = preg_match("/(090|080|050)-{0,1}[0-9]{4}-{0,1}[0-9]{4}/u", $phone_number);
+		if ($result) {
+				echo "{$phone_number}は条件に一致するナンバーです。<br>";
+		} else {
+				echo "{$phone_number}は条件に一致しないナンバーです。<br>";
+		}
+}
+echo "<br>";
+
+// クレジットカードの番号の真偽を判断
+$credit_cards = ['1234123412341234', '1234 1234 1234 1234', '1234 5678 9012 3456', '1234-1234-1234-1234'];
+
+$pattern     = "/\d{4}\s?\d{4}\s?\d{4}\s?(\d{4})$/";
+$replacement = "**** **** **** $1";
+foreach ($credit_cards as $credit_card) {
+		$result = preg_match($pattern, $credit_card);
+		if ($result) {
+				$replaced_result = preg_replace($pattern, $replacement, $credit_card);
+				echo "条件に一致するナンバーです。<br>";
+				echo "→{$replaced_result}を保存しました。<br>";
+		} else {
+				echo "{$credit_card}は条件に一致しないナンバーです。<br>";
+		}
+}
+echo "<br>";
